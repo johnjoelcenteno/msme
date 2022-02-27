a1df98a5f59420ff92aa271705bbff6c57110f65
Joel John Centeno <johnjoelcenteno@gmail.com>
2022-02-27 05:12:55 -0800

    fixing dashboard design

--- assets/custom_js/dashboard-processes.js
+++ assets/custom_js/dashboard-processes.js
@@ -6,26 +6,49 @@ $(document).ready(function(){
 	const vacantPositionToRateArray = JSON.parse(employeeInfo.vacant_position_to_rate);
 	const openPositionsForInterviewArray = JSON.parse(positionsForInterview);
 	
+	const dashboardDropDowns = []; // for the titles of the thumbnails in dashboard
 	let plantillaNumbersToRate = []; // here is the collection of plantilla numbers that the current user can rate
 	openPositionsForInterviewArray.forEach((openInterviewElement) => {
+		const ddTitlePositionTitle = openInterviewElement.position_title; // first element for dashboard dd title
+		const ddTitlePlantillaItemNo = openInterviewElement.plantilla_item_no; // second element for dashboard dd title
+		const salaryJobPayScale = openInterviewElement.Salary_job_pay_scale;
+
 		let isForInterviewOfficeName = openInterviewElement.office_name;
 		let isForInterviewProvince = openInterviewElement.province;
 		let plantillaNumberOfOpenInterview = openInterviewElement.plantilla_item_no;
-		console.log(`Is for interview office name: ${isForInterviewOfficeName}`, `Is for interview province name: ${isForInterviewProvince}`);
 		vacantPositionToRateArray.forEach((isInVacantPositionElement) => {
 			let isInVacantPositionOfficeName = isInVacantPositionElement.office_name;
 			let isInVacantPositionProvince = isInVacantPositionElement.province;
-			console.warn(`isInVacantPositionOfficeName: ${isInVacantPositionOfficeName}`, `isInVacantPositionProvince: ${isInVacantPositionProvince}`);
 			if(isInVacantPositionOfficeName == isForInterviewOfficeName && isInVacantPositionProvince == isForInterviewProvince){
 				plantillaNumbersToRate.push(plantillaNumberOfOpenInterview);
-				console.log(plantillaNumberOfOpenInterview, 'nakuha ko ito');
+
+				// configure dashboard drop down
+				let dashboardDdTitleOfficeName = isInVacantPositionOfficeName; // third dd element
+				let dashboardDdTitleProvinceName = isInVacantPositionOfficeName; // fourth dd element
+				
+				const objForDashboardDd = {
+					title: `${ddTitlePositionTitle} : ${ddTitlePlantillaItemNo} : ${dashboardDdTitleOfficeName} - ${dashboardDdTitleProvinceName}`,
+					salaryGrade: salaryJobPayScale
+				};
+				
+				dashboardDropDowns.push(objForDashboardDd);
 			}
 		});		
 	});
-	console.log(plantillaNumbersToRate);
-	const allApplicants = JSON.parse(applicantsTable);
-	plantillaNumbersToRate.forEach((canRatePlantilla) => {
-		
+	//plantillaNumbersToRate 
+	const summaryOfAllApplicantsThatCanBeRated = [];
+	const allApplicants = JSON.parse(applicantsTable); // lahat ng applicants
+	allApplicants.forEach(applicant => {
+		let positionAppliedForOfTheApplicant = applicant.position_applied_for.split(',');
+
+		positionAppliedForOfTheApplicant.forEach(positionAppliedForOfTheApplicantElement => {
+			if(plantillaNumbersToRate.includes(positionAppliedForOfTheApplicantElement)) summaryOfAllApplicantsThatCanBeRated.push(applicant);
+		});
 	});
+	
+	// summaryOfAllApplicantsThatCanBeRated is the list of applicant element that the user can rate
+	// console.log(plantillaNumbersToRate); dito mo makukuha lahat ng pwedeng ma rate ng naka login. 
+	// console.log(summaryOfAllApplicantsThatCanBeRated);
+	console.log(dashboardDropDowns); //summary of titles for the thumbnail of dashboard
 });
 //Office of the Regional Executive Director, Regional Office
